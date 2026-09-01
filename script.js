4c0c97ccd962c53b0415fa73d92a705e9066636c
leticiapatriciolima-glitch <leticia.patricio.lima@escola.pr.gov.br>
2026-09-01 10:56:49 -0300

    .

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const botao = document.querySelector("button");
-botao.addEventListener("click"botaoClicado);
-
+botoes.forEach(function(botao) {
+    let curtiu = false;
+botao.addEventListener("click", botaoClicado);
 function botaoClicado () {
 console.log("fui clicado");
 let texto = botao querySelector("span");
-texto.textConect++;
+if (curtiu === false) {
     
 }
+texto.textConect++; 
+}
+});
